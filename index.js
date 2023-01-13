5b05aae87fb8e51dc4824ef10862b1db617e84b2
mnnkhndlwl <khandelwalmanan05@gmail.com>
2023-01-13 12:52:17 +0530

    cors

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ import cors from "cors";
 const app = express();
 
 dotenv.config();
-const corsOptions = {
-  origin: 'http://localhost:3000',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: 'http://localhost:3000',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
+app.use(cors());
 // to connect our application to mongodb
 const connect = async () => {
   try {
